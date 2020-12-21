d9421c8855d208292732c31a5af417c8efec9c8f
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-12-21 17:41:39 +0530

    updated slot test

--- test/com/step/parking/SlotTest.java
+++ test/com/step/parking/SlotTest.java
@@ -1,4 +1,30 @@
+package com.step.parking;
+
+import com.step.record.SlotRecord;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class SlotTest {
-  
+    @Test
+    public void shouldConfirmIfSlotIsAvailable() {
+        Slot slot = new Slot(SlotStatus.EMPTY);
+        
+        assertTrue(slot.isAvailable());
+    }
+    
+    @Test
+    public void shouldNotConfirmIfSlotIsAvailable() {
+        Slot slot = new Slot(SlotStatus.FILLED);
+        
+        assertFalse(slot.isAvailable());
+    }
+    
+    @Test
+    public void shouldGenerateSlotRecord() {
+        Slot slot = new Slot(SlotStatus.EMPTY);
+        SlotRecord expected = slot.generateSlotRecord();
+        
+        assertEquals(expected, new SlotRecord(SlotStatus.EMPTY));
+    }
 }
